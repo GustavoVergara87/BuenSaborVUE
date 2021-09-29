a8919c143b55349129131a769aeda505ae07c80e
Edgardo Gustavo Vergara - UTN <edgardo.vergara@alumnos.frm.utn.edu.ar>
2021-09-28 21:23:50 -0300

    Update RecetaController.js

--- src/services/RecetaController.js
+++ src/services/RecetaController.js
@@ -3,8 +3,10 @@ import store from "../store/index"
 export async function addDetalleReceta(detalleReceta) {
     const response = await fetch("/api/DetallesRecetas", {
         method: "POST",
-        headers: { "Content-type": "application/json",
-        'Authorization': 'Bearer ' + store.getters.traerToken },
+        headers: {
+            "Content-type": "application/json",
+            'Authorization': 'Bearer ' + store.getters.traerToken
+        },
         body: JSON.stringify(detalleReceta),
     });
     const responseJson = await response.json();
@@ -13,23 +15,24 @@ export async function addDetalleReceta(detalleReceta) {
 
 export async function deleteDetalleReceta(id) {
 
-    const detalleABorrar = await fetch("/api/DetallesRecetas/" + id);
+    const detalleABorrar = await fetch("/api/DetallesRecetas/" + id, {
+        headers: {
+            'Authorization': 'Bearer ' + store.getters.traerToken
+        }
+    });
     const detalleABorrarJson = await detalleABorrar.json();
 
     detalleABorrarJson.disabled = true;
-    
+
     await fetch(`/api/DetallesRecetas/${id}`, { //disable el articulo del backEnd
         method: "PUT",
-        headers: { "Content-type": "application/json",
-        'Authorization': 'Bearer ' + store.getters.traerToken },
+        headers: {
+            "Content-type": "application/json",
+            'Authorization': 'Bearer ' + store.getters.traerToken
+        },
         body: JSON.stringify(detalleABorrarJson),
     });
 
-    // await fetch(`/api/DetallesRecetas/${id}`, { //disable el articulo del backEnd
-    //     method: "DELETE",
-    //     headers: { "Content-type": "application/json" },
-
-    // });
 }
 
 
