cfa923b929e73f2ec87c0bd2587bff46a9d3c7bc
bdefilippi <bruno.de@alumnos.frm.utn.edu.ar>
2021-10-16 18:11:35 -0300

    Cambios en config para conectarse online

--- vue.config.js
+++ vue.config.js
@@ -1,26 +1,28 @@
-const path = require('path');
-// const serverURL = 'http://elbuensabor.ddns.net:82'
-const serverURL = 'https://localhost:44350'
+const path = require("path");
+const serverURL = "http://elbuensabor.ddns.net:82";
+//const serverURL = "https://localhost:44350";
+// const serverURL = "http://192.168.1.35:82";
 
 module.exports = {
-  publicPath: process.env.NODE_ENV === 'production'
-    ? '/BuenSaborVUE/'
-    : '/',
-  devServer: {
-    proxy: {
-      '/api': {
-        target: serverURL,
-      },
-      '/image': {
-        target: serverURL + '/api/Images',
-        pathRewrite: { '^/image': '' }
-      },
-      '/notificacionesHub': {
-        target: serverURL + '/notificacionesHub',
-        pathRewrite: { '^/notificacionesHub': '' }
-      }
-    },
-  },
-
-
-}
+	publicPath: process.env.NODE_ENV === "production" ? "./" : "/",
+	devServer: {
+		headers: {
+			"Access-Control-Allow-Private-Network": true,
+			"Access-Control-Allow-Origin": "*",
+		},
+		disableHostCheck: true,
+		proxy: {
+			"/api": {
+				target: serverURL,
+			},
+			"/image": {
+				target: serverURL + "/api/Images",
+				pathRewrite: { "^/image": "" },
+			},
+			"/notificacionesHub": {
+				target: serverURL + "/notificacionesHub",
+				pathRewrite: { "^/notificacionesHub": "" },
+			},
+		},
+	},
+};
